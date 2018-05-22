5eac1594ccf8495e30cff94d7fb249644cba0e42
lassesteffen <lasse.steffen@t-online.de>
2018-05-22 15:00:10 +0200

    Added roboto support [skip ci]

---
+++ build-support/copyfonts.sh
@@ -0,0 +1,3 @@
+mkdir -p /home/travis/smalltalkCI-master/_builds/Scamper/fonts
+cp -R $TRAVIS_BUILD_DIR/build-support/fonts/* /home/travis/smalltalkCI-master/_builds/Scamper/fonts
+chmod -R 777 /home/travis/smalltalkCI-master/_builds/Scamper/fonts/*
