98f7902d32cae90b35aeae42ba28524a4a5e5578
mschroederi <me@matspoerschke.de>
2018-05-26 19:34:47 +0200

    Travis support (#56)
    * Update copyfonts.sh
    
    * updated baseline
    
    * fixed issue with MDClickable deleteRipple
    
    * update travis.yml [ci skip]

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -1,3 +1,5 @@
+echo $TRAVIS_OS_NAME
+
 IMAGE_PATH="/home/travis/smalltalkCI-master/_builds/Roboto/"
 
 if [ "$TRAVIS_OS_NAME" == "osx" ];
@@ -7,3 +9,5 @@ fi;
 mkdir -p $IMAGE_PATH
 cp -R $TRAVIS_BUILD_DIR/build-support/fonts/* $IMAGE_PATH
 chmod -R 777 $IMAGE_PATH
+
+ls -lisa $IMAGE_PATH
