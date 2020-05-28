530b55df0ab416c46de20751f0a1625ac1e20387
Jonas Wanke <contact@jonas-wanke.com>
2020-05-28 07:37:41 +0200

    build: adapt font copying to GitHub Actions

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -1,13 +1,13 @@
-echo $TRAVIS_OS_NAME
+echo $ACTIONS_OS
 
-IMAGE_PATH="/home/travis/smalltalkCI-master/_builds/Roboto/"
+IMAGE_PATH="/home/runner/smalltalkCI-master/_builds/Roboto/"
 
-if [ "$TRAVIS_OS_NAME" == "osx" ];
-  then IMAGE_PATH="/Users/travis/smalltalkCI-master/_builds/Roboto/";
+if [ "$ACTIONS_OS" == "macos-latest" ];
+  then IMAGE_PATH="/Users/runner/smalltalkCI-master/_builds/Roboto/";
 fi;
 
 mkdir -p $IMAGE_PATH
-cp -R $TRAVIS_BUILD_DIR/build-support/fonts/* $IMAGE_PATH
+cp -R ./fonts/* $IMAGE_PATH
 chmod -R 777 $IMAGE_PATH
 
 ls -lisa $IMAGE_PATH
